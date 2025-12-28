7009d9c2ecd964a9bd947e966e4d9419ec4bd3ce
hungchi0222 <charlie950222@gmail.com>
2025-12-29 03:24:16 +0800

    trying village_game

--- village_game/config.py
+++ village_game/config.py
@@ -20,7 +20,6 @@ COLOR_GOLD = (255, 215, 0)
 COLOR_DEATH = (100, 100, 100)   
 COLOR_DAMAGE = (255, 50, 50)  # [新增] 傷害數字顏色
 COLOR_HEAL = (100, 255, 255)  # [新增] 治療/修復顏色
-
 # --- 圖片設定 ---
 IMG_DIR = "assets"
 IMG_HERO = "hero.png"
